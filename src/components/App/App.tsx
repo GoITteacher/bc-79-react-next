7d132358acee29415178ed07528993acc977d881
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2026-03-17 09:24:23 +0200

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,10 +6,13 @@ import TaskForm from "../TaskForm/TaskForm";
 import SearchBox from "../SearchBox/SearchBox";
 import SortFilter from "../SortFilter/SortFilter";
 import css from "./App.module.css";
+import UserList from "../UserList/UserList";
 
 export default function App() {
   return (
     <div className={css.container}>
+      <UserList />
+      <hr />
       <header className={css.header}>
         <SearchBox value={""} onSearch={() => {}} />
         <SortFilter />
@@ -17,6 +20,7 @@ export default function App() {
           Create task
         </button>
       </header>
+
       <strong className={css.loading}>Loading tasks...</strong>
       <TaskList tasks={[]} />
     </div>
---
+++ src/components/UserList/UserList.tsx
@@ -0,0 +1,7 @@
+import css from "./UserList.module.css";
+
+const UserList = () => {
+  return <div className={css["user-list-container"]}>Hello</div>;
+};
+
+export default UserList;
